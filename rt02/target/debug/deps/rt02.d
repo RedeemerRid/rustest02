f2e552b2212989266dde8e04dba1858aa1f2e015
RedeemerRid <akhmatovich@gmail.com>
2024-08-14 16:26:36 +0300

    first commit rt02

---
+++ rt02/target/debug/deps/rt02.d
@@ -0,0 +1,5 @@
+D:\RUST\1\rustest02\rt02\target\debug\deps\rt02.exe: src/main.rs
+
+D:\RUST\1\rustest02\rt02\target\debug\deps\rt02.d: src/main.rs
+
+src/main.rs:
---
+++ rt02/target/debug/rt02.d
@@ -0,0 +1,1 @@
+D:\RUST\1\rustest02\rt02\target\debug\rt02.exe: D:\RUST\1\rustest02\rt02\src\main.rs
